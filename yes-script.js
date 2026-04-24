371fb2a7374a8de484d24a71af03f23d05ecb8d9
MiguelJR23 <r0b3rts2332@gmail.com>
2026-04-23 23:02:55 -0300

    Slider tbm

--- yes-script.js
+++ yes-script.js
@@ -61,3 +61,74 @@ function toggleMusic() {
         document.getElementById('music-toggle').textContent = '🔊'
     }
 }
+
+const popup = document.getElementById("volume-popup");
+const control = document.getElementById("music-control");
+const music = document.getElementById('bg-music')
+const slider = document.getElementById("volume-slider");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+
+    heart.innerText = "❤";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (16 + Math.random() * 20) + "px";
+
+    const duration = 4 + Math.random() * 4;
+    heart.style.animationDuration = duration + "s";
+
+    container.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, duration * 1000);
+}
+
+setInterval(createHeart, 300);
+
+slider.addEventListener("input", () => {
+    const value = slider.value;
+    // volume
+    music.volume = value / 100;
+    // escala do coração (0.5 até 1.5)
+    const scale = 0.5 + (value / 100);
+    heart.style.transform = `scale(${scale})`;
+    // opacidade
+    heart.style.opacity = 0.5 + (value / 200);
+});
+
+document.getElementById("music-toggle").addEventListener("click", () => {
+    control.classList.toggle("active");
+});
+
+// quando entra na área do botão
+control.addEventListener("mouseenter", () => {
+    clearTimeout(hideTimeout);
+    control.classList.add("active");
+});
+
+// quando sai da área do botão
+control.addEventListener("mouseleave", () => {
+    hideTimeout = setTimeout(() => {
+        control.classList.remove("active");
+    }, 2500); // tempo que ele continua visível (2.5s)
+});
+
+function showPopup() {
+    clearTimeout(hideTimeout);
+    control.classList.add("active");
+}
+
+function hidePopupWithDelay() {
+    hideTimeout = setTimeout(() => {
+        control.classList.remove("active");
+    }, 2500);
+}
+
+control.addEventListener("mouseenter", showPopup);
+control.addEventListener("mouseleave", hidePopupWithDelay);
+popup.addEventListener("mouseenter", showPopup);
+popup.addEventListener("mouseleave", hidePopupWithDelay);
+slider.addEventListener("input", showPopup);
